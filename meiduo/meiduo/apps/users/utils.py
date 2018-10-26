f7af3f2320a7072731f90b1d51b5e0b682b0c7dd
zq <9778862522@qq.com>
2018-10-26 10:58:37 +0800

    自定义认证机制，添加手机号登陆

--- meiduo/meiduo/apps/users/utils.py
+++ meiduo/meiduo/apps/users/utils.py
@@ -1,5 +1,27 @@
+import re
+
+from django.contrib.auth.backends import ModelBackend
+
+from users.models import User
+
+
 def jwt_response_payload_handler(token, user=None, request=None):
     '''自定义ｊｗｔ返回结果'''
     return {
         "token": token, "user_id": user.id, "username": user.username
     }
+
+
+class UsernameMobileAuthBackend(ModelBackend):
+    '''对判断进行重写，添加手机号登陆'''
+
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        try:
+            if re.match(r"^1[3-9]\d{9}$", username):
+                user = User.objects.get(mobile=username)
+            else:
+                user = User.objects.get(username=username)
+        except:
+            user = None
+        if user and user.check_password(password):
+            return user
--- meiduo/meiduo/settings/dev.py
+++ meiduo/meiduo/settings/dev.py
@@ -208,3 +208,6 @@ CORS_ALLOW_CREDENTIALS = True  # 允许携带cookie
 JWT_AUTH = {
     "JWT_RESPONSE_PAYLOAD_HANDLER": "users.utils.jwt_response_payload_handler"  # 对ｊｗｔ结果重写
 }
+AUTHENTICATION_BACKENDS = [
+    'users.utils.UsernameMobileAuthBackend',
+]  # 使用我们自己定义的认证后端
