9d4b355a038e6af8decdab326cabe663b94f4e59
zq <9778862522@qq.com>
2018-11-02 11:55:02 +0800

    面包屑导航

--- meiduo/meiduo/apps/goods/urls.py
+++ meiduo/meiduo/apps/goods/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
-from .views import SKUListView
+from .views import *
 
 urlpatterns = [
-    url(r"^categories/(?P<pk>\d+)/skus/$", SKUListView.as_view())
+    url(r"^categories/(?P<pk>\d+)/skus/$", SKUListView.as_view()),
+    url(r"^categories/(?P<pk>\d+)/$", GoodCategorieView.as_view()),
 ]
--- meiduo/meiduo/apps/goods/views.py
+++ meiduo/meiduo/apps/goods/views.py
@@ -3,8 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 from rest_framework.filters import OrderingFilter
 from rest_framework.generics import ListAPIView
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-from goods.models import SKU
+from goods.models import SKU, GoodsCategory
 from goods.serializers import SKUSerializer
 from goods.utils import StandardResultsSetPagination
 
@@ -20,3 +22,15 @@ class SKUListView(ListAPIView):
     pagination_class = StandardResultsSetPagination
     filter_backends = [OrderingFilter]
     ordering_fields = ['create_time', 'price', 'sales']
+
+
+class GoodCategorieView(APIView):
+    def get(self, request, pk):
+        cat3 = GoodsCategory.objects.get(pk=pk)
+        cat2 = cat3.parent
+        cat1 = cat2.parent
+        return Response({
+            'cat1': cat1.name,
+            'cat2': cat2.name,
+            'cat3': cat3.name,
+        })
